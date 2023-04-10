e13e2f34546228a23f4e60ee121c3c186fc974d8
humanG0D <lhulhumaung@gmail.com>
2023-04-11 03:17:58 +0800

    update

--- pimpmykali.sh
+++ pimpmykali.sh
@@ -1,90 +1,110 @@
 #!/bin/bash
 # hum4ng0d version of kali
 
-echo "pimpmykali v0.1"
-echo "Start of a new adventure..."
+# Define color codes
+blue='\033[0;34m'
+green='\033[0;32m'
+reset='\033[0m'
+
+echo "${blue}pimpmykali v0.1${reset}"
+echo "${blue}Start of a new adventure...${reset}"
 
 user="kali"
 sudo chown -R $user:$user /opt
+echo "${green}[+] Successfully set chown of /opt to $user.${reset}"
 
 rm -rf ~/Public ~/Videos ~/Pictures ~/Music ~/Documents ~/Templates
-mkdir ~/www
-mkdir /opt/pimpmykali
+echo "${green}[+] Successfully deleted useless folders.${reset}"
+mkdir ~/www && mkdir /opt/pimpmykali
+curl -o /opt/pimpmykali/kali-new-16x9.png https://github.com/hum4nG0D/pimpmykali/raw/main/images/kali-new-16x9.png
+echo "${green}[+] Successfully downloaded wallpaper to /opt/pimpmykali.${reset}"
 
-sections=$(awk '/----/{print NR}' list.txt)
-sections+=($(wc -l < list.txt))
+# List of packages to install
+packages=(
+    gedit
+    golang-go
+    seclists
+    curl
+    dnsrecon
+    enum4linux
+    feroxbuster
+    gobuster
+    impacket-scripts
+    nbtscan
+    nikto
+    nmap
+    onesixtyone
+    oscanner
+    redis-tools
+    smbclient
+    smbmap
+    snmpwalk
+    sslscan
+    svwar
+    tnscmd10g
+    whatweb
+    wkhtmltopdf
+    htop
+    xrdp
+)
 
-apt_packages=$(sed -n "${sections[0]},$((sections[1]-1))p" list.txt)
-while read package; do
-    echo "[i] Installing $package..."
-    sudo apt-get -y install $package
-    sudo apt update
-    echo "[+] Successfully installed $package."
-done <<< "$apt_packages"
+sudo apt update
 
-go_packages=$(sed -n "${sections[1]},$((sections[2]-1))p" list.txt)
-while read package; do
-    echo "[i] Installing $package..."
-    go install $package
-    echo "[+] Successfully installed $package."
-done <<< "$go_packages"
-
-installer_urls=$(sed -n "${sections[2]},$((sections[3]-1))p" list.txt)
-for installer in $installer_urls; do
-    echo "[i] Downloading $installer..."
-    wget -q -P /tmp $installer
-    installer_name=$(echo $installer | awk -F/ '{print $NF}')
-    echo "[i] Installing $installer_name..."
-    sudo dpkg -i /tmp/$installer_name
-    echo "[+] Successfully installed $installer_name."
-    echo "[+] Removing $installer_name..."
-    sudo rm /tmp/$installer_name
-    echo "[+] Successfully removed $installer_name."
+for package in "${packages[@]}"
+do
+  sudo apt install -y "$package"
+  echo "${green}[+] Successfully installed $package.${reset}"
 done
 
-github_repos=$(sed -n "${sections[3]},$((sections[4]-1))p" list.txt)
-for repo in $github_repos; do
-    repo_name=$(echo $repo | awk -F/ '{print $NF}' | sed 's/.git//')
-    echo "[i] Installing $repo_name..."
-    git clone $repo /opt/$repo_name
-    if [ -f /opt/$repo_name/requirements.txt ]; then
-        echo "[i] Installing requirements for $repo_name..."
-        pip install -r /opt/$repo_name/requirements.txt
-        echo "[+] Successfully installed requirements for $repo_name."
-    fi
-    echo "[+] Successfully installed $repo_name."
-done
+sudo apt update
 
-python_modules=$(sed -n "${sections[4]},$((sections[5]-1))p" list.txt)
-for module in $python_modules; do
-    echo "[i] Installing $module..."
-    pip install $module
-    echo "[+] Successfully installed $module."
-done
+# List of repositories to clone
+repositories=(
+    https://github.com/t3l3machus/hoaxshell.git
+    https://github.com/t3l3machus/Villain.git
+    https://github.com/s0md3v/XSStrike.git
+    https://github.com/hum4nG0D/simple-HTTPS-server.git
+    https://github.com/hum4nG0D/lfi-fuzz.git
+    https://github.com/killmongar1996/sqlTinj.git
+    https://github.com/enjoiz/XXEinjector.git
+    https://github.com/shelld3v/JSshell.git
+    https://github.com/swisskyrepo/GraphQLmap.git
+    https://github.com/21y4d/nmapAutomator.git
+    https://github.com/cyberark/MITM_Intercept.git
+    https://github.com/jrmdev/mitm_relay.git
+    https://github.com/9emin1/charlotte.git
+)
 
-file_urls=$(sed -n "${sections[5]}p" list.txt)
-for file_url in $file_urls; do
-    echo "[+] Downloading $file_url..."
+for repository in "${repositories[@]}"
+do
+  # Extract repository name from URL
+  repo_name="${repository##*/}"
+  repo_name="${repo_name%.*}"
+  
+  if [ -d "/opt/$repo_name" ]
+  then
+    echo "${blue}[i] $repo_name already exists in /opt directory. Skipping cloning step.${reset}"
+  else
+    cd /opt
+    echo "${blue}[i] Cloning $repository...${reset}"
+    git clone "$repository"
+  fi
+  
+  if [ -f "/opt/$repo_name/requirements.txt" ]
+  then
+    echo "${blue}[i] Installing requirements for $repo_name...${reset}"
+    python3 -m pip install -r "/opt/$repo_name/requirements.txt"
+    echo "${green}[+] Successfully installed requirements for $repo_name.${reset}"
+  fi
+done
 
 #Manual steps
+#Cloning tplmap
+git clone https://github.com/epinna/tplmap.git
+echo "${green}[+] Successfully cloned tplmap.${reset}"
+
 #creating python2 virtual env
 python3 -m virtualenv ~/py2
+echo "${green}[+] Successfully created py2 virtualenv.${reset}"
 python3 -m virtualenv --python=/usr/bin/python2 ~/py2
-
-#Cloning tplmap
-source ~/py2/bin/activate
-git clone https://github.com/epinna/tplmap.git /opt
-echo "[+] Successfully cloned tplmap"
-python -m pip install -r /opt/tplmap/requirements.txt
-deactivate
-
-#Setting workspaces
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 9
-
-#Setting wallpaper
-wget -p /opt/pimpmykali https://github.com/hum4nG0D/pimpmykali/raw/main/images/kali-new-16x9.png 
-gsettings set org.gnome.desktop.background picture-uri file:///opt/pimpmykali/kali-new-16x9.png
-
-
-
-
+echo "${green}[+] Successfully set python2 for py2.${reset}"
